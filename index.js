a069c4e6327c7f3f1cca524717bb988acab7b8de
Vladislav Georgiev <vlad.georgiev@outlook.com>
2019-02-26 10:44:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,15 @@
 function happyholidays() {
   return "Happy Holidays!"
 }
+
+function happyHolidaysTo(name) {
+  return `Happy holidays ${name}!`
+}
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days unil ${holiday}!`
+}
